66064548298d7fae700b20d3d555eef8ad670ae2
Keita Nakamura <keita.nakamura.1109@gmail.com>
2021-11-08 11:07:30 +0900

    Simplify tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,8 @@
 using Grapher
 using Test
 
-using Grapher: merge_recursive
+axis_options(options::Grapher.Options) = Grapher.merge_recursive(Grapher.default_axis_options(), options)
+plot_options(options::Grapher.Options) = Grapher.merge_recursive(Grapher.default_plot_options(), options)
 
 
 @testset "Axis options" begin
@@ -11,44 +12,44 @@ using Grapher: merge_recursive
     @test ax.options == Grapher.default_axis_options()
 
     ax = plot([1,2,3], [4,5,6],
-              xlabel = "abc",
-              not_supported_option = "abc",
-              axis_options = @pgf{xlabel = "def",
-                                  ylabel = "def",}, # this should be overwritten by above `xlabel` option
+              xlabel = "foo",
+              not_supported_option = "foo",
+              axis_options = @pgf{xlabel = "bar",
+                                  ylabel = "bar",}, # this should be overwritten by above `xlabel` option
              )
     @test isa(ax, Grapher.Axis)
     @test length(ax.contents) == 1
-    @test ax.options == merge_recursive(@pgf{xlabel = "abc", ylabel = "def"}, Grapher.default_axis_options())
+    @test ax.options == axis_options(@pgf{xlabel = "foo", ylabel = "bar"})
 
     @testset "size" begin
         ax = plot([1,2,3], [4,5,6], size = "landscape")
-        @test ax.options == merge_recursive(@pgf{width = "80mm", height = "50mm"}, Grapher.default_axis_options())
+        @test ax.options == axis_options(@pgf{width = "80mm", height = "50mm"})
         ax = plot([1,2,3], [4,5,6], size = "portrait")
-        @test ax.options == merge_recursive(@pgf{width = "50mm", height = "80mm"}, Grapher.default_axis_options())
+        @test ax.options == axis_options(@pgf{width = "50mm", height = "80mm"})
         ax = plot([1,2,3], [4,5,6], size = "square")
-        @test ax.options == merge_recursive(@pgf{width = "80mm", height = "80mm"}, Grapher.default_axis_options())
+        @test ax.options == axis_options(@pgf{width = "80mm", height = "80mm"})
     end
 
     @testset "xtick, ytick, ztick" begin
         ax = plot(["a","b","c"], [4,5,6], xtick = ["c","b","a"])
-        @test ax.options == merge_recursive(@pgf{symbolic_x_coords = ["c","b","a"], xtick = "data"}, Grapher.default_axis_options())
+        @test ax.options == axis_options(@pgf{symbolic_x_coords = ["c","b","a"], xtick = "data"})
     end
 end
 
 @testset "Legend" begin
     ax = plot([1,2,3], [4,5,6],
-              legend = "abc",
+              legend = "foo",
               legend_pos = "north east",
              )
     @test isa(ax, Grapher.Axis)
     @test length(ax.contents) == 2
     @test ax.contents[2] isa Grapher.LegendEntry
-    @test ax.options == merge_recursive(@pgf{legend_pos = "north east"}, Grapher.default_axis_options())
+    @test ax.options == axis_options(@pgf{legend_pos = "north east"})
 
     ax = plot([1,2,3], [4,5,6],
               legend_pos = (0.5, 0.0),
              )
-    @test ax.options == merge_recursive(@pgf{legend_style = {at = Grapher.Coordinate(0.5, 0.0)}}, Grapher.default_axis_options())
+    @test ax.options == axis_options(@pgf{legend_style = {at = Grapher.Coordinate(0.5, 0.0)}})
 end
 
 @testset "Plot options" begin
@@ -64,32 +65,32 @@ end
                                   marker = "o",}, # given option is passed without modifications in `plot_options`
              )
     plt = only(ax.contents)
-    @test plt.options == merge_recursive(@pgf{color = "red", marker = "o"}, Grapher.default_plot_options())
+    @test plt.options == plot_options(@pgf{color = "red", marker = "o"})
 
     @testset "marker" begin
         ax = plot([1,2,3], [4,5,6], marker = "o")
         plt = only(ax.contents)
-        @test plt.options == merge_recursive(@pgf{mark = "o"}, Grapher.default_plot_options())
+        @test plt.options == plot_options(@pgf{mark = "o"})
 
         ax = plot([1,2,3], [4,5,6], marker = nothing)
         plt = only(ax.contents)
-        @test plt.options == merge_recursive(@pgf{no_marks}, Grapher.default_plot_options())
+        @test plt.options == plot_options(@pgf{no_marks})
     end
 
     @testset "line_style" begin
         ax = plot([1,2,3], [4,5,6], line_style = "dotted")
         plt = only(ax.contents)
-        @test plt.options == merge_recursive(@pgf{dotted}, Grapher.default_plot_options())
+        @test plt.options == plot_options(@pgf{dotted})
     end
 
     @testset "line_width" begin
         ax = plot([1,2,3], [4,5,6], line_width = "thick")
         plt = only(ax.contents)
-        @test plt.options == merge_recursive(@pgf{thick}, Grapher.default_plot_options())
+        @test plt.options == plot_options(@pgf{thick})
 
         ax = plot([1,2,3], [4,5,6], line_width = nothing)
         plt = only(ax.contents)
-        @test plt.options == merge_recursive(@pgf{draw = "none"}, Grapher.default_plot_options())
+        @test plt.options == plot_options(@pgf{draw = "none"})
     end
 
     @testset "on/off options" begin
@@ -97,7 +98,7 @@ end
             @eval begin
                 ax = plot([1,2,3], [4,5,6], $name = true)
                 plt = only(ax.contents)
-                @test plt.options == merge_recursive(@pgf{$name}, Grapher.default_plot_options())
+                @test plt.options == plot_options(@pgf{$name})
             end
         end
     end
