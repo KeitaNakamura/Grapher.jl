4b2c9c4e9c616ade721999d89ac16174318873ff
Keita Nakamura <keita.nakamura.1109@gmail.com>
2023-08-11 19:59:04 +0900

    Fix preamble for siunitx

--- src/Grapher.jl
+++ src/Grapher.jl
@@ -15,9 +15,8 @@ function __init__()
         push!(PGFPlotsX.CUSTOM_PREAMBLE, "\\usepackage{unicode-math}")
         push!(PGFPlotsX.CUSTOM_PREAMBLE, "\\setmainfont{STIX Two Text}")
         push!(PGFPlotsX.CUSTOM_PREAMBLE, "\\setmathfont{STIX Two Math}")
-    else
-        push!(PGFPlotsX.CUSTOM_PREAMBLE, "\\usepackage[group-separator={,}]{siunitx}")
     end
+    push!(PGFPlotsX.CUSTOM_PREAMBLE, "\\usepackage[group-separator={,}]{siunitx}")
 end
 
 export
